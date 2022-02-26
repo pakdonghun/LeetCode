5fdd4e07b6ef9eafc349190ff69c85808169914e
pakdonghun <66273924+pakdonghun@users.noreply.github.com>
2022-02-26 09:29:31 +0900

    Time: 273 ms (66.73%), Space: 21 MB (95.07%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -7,7 +7,7 @@ class Solution:
             p = p * nums[i]
             
         p = 1
-        for i in range(len(nums) - 1, 0 - 1, -1):
+        for i in range(len(nums) - 1, - 1, -1):
             out[i] = out[i] * p
             p = p * nums[i]
         return out
